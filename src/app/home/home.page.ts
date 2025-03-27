238c8b100894a8b54e53ae46335a57851ce3714d
Alerop <alejandrorp93@gmail.com>
2025-03-27 19:27:30 +0100

    #2 using npm publish works the use

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { OnlyPrototypeTComponent, OnlyPrototypeTService } from '@Only-Prototype-T';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, OnlyPrototypeTComponent],
 })
 export class HomePage {
-  constructor() {}
+
+  s = inject(OnlyPrototypeTService);
+
+  constructor() {
+  }
 }
